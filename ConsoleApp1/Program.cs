ee220c08ec31bd3b58f83d08b60450d78f0b9d29
Aman-ABJIMA <aman.singh@abjima.com>
2023-01-02 18:33:02 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+
+
+         obj.list2.Add(10);
+            obj.list2.Remove(10);
+        foreach(var item in obj.list2)
+            {
+                Console.WriteLine(item);
+            }
+           
+        }
+    }
+}
